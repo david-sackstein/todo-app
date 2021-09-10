fc52e37263150f7a1c89ec0cab31ca2116bbb492
David Sackstein <davids@codeprecise.com>
2021-09-11 01:49:58 +0300

    Step 8 - Allow deleting the list

--- src/app/components/list-view/list-view.component.ts
+++ src/app/components/list-view/list-view.component.ts
@@ -13,15 +13,16 @@ import { StateService } from '../../modules/core/services/state-service';
   styleUrls: ['./list-view.component.scss'],
 })
 export class ListViewComponent implements OnInit {
-
   todoList$: Observable<TodoList>;
   todoItems$: Observable<TodoItem[]>;
   newTaskControl: FormControl;
+  isDeleting = false;
 
   constructor(
+    private router: Router,
     private stateService: StateService,
     formBuilder: FormBuilder,
-    route: ActivatedRoute,
+    route: ActivatedRoute
   ) {
     this.todoList$ = route.params.pipe(
       switchMap((params) => {
@@ -36,13 +37,38 @@ export class ListViewComponent implements OnInit {
       })
     );
 
-    this.newTaskControl = formBuilder.control('', [Validators.required, Validators.minLength(10)]);
+    this.newTaskControl = formBuilder.control('', [
+      Validators.required,
+      Validators.minLength(10),
+    ]);
   }
 
   addTask(id: number) {
     this.stateService.AddTodoItem(id, this.newTaskControl.value);
-    this.newTaskControl.reset()
+    this.newTaskControl.reset();
   }
 
-  ngOnInit(): void { }
+  newList() {
+    this.router.navigateByUrl("/lists/-1/edit");
+  }
+
+  editList(id: number) {
+    this.router.navigateByUrl(`/lists/${id}/edit`);
+  }
+
+  delete() {
+    this.isDeleting = true;
+  }
+
+  cancelDelete() {
+    this.isDeleting = false;
+  }
+
+  confirmDelete(id: number) {
+    this.isDeleting = false;
+    this.stateService.DeleteList(id);
+    this.router.navigateByUrl('/home');
+  }
+
+  ngOnInit(): void {}
 }
